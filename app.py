364ac292f37c339c34e1aa150e3dc10984f7025c
UtkarshaNerle <utkarshanerle@gmail.com>
2025-03-28 19:05:45 +0530

    initial files

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
-from flask import Flask
+from flask import Flask,render_template
 
 app=Flask(__name__)
 
-@app.route('/')
+@app.route('/home')
 def home():
     return "welcome to home"
+
+@app.route('/details')
+def details():
+    return render_template("index.html")
